726e9a59677cf317fb9211a8caa301df60a778f9
Somon <somon.olimzoda1@gmail.com>
2023-02-22 02:16:01 +0900

    Update SQL script.sql

--- SQL script.sql
+++ SQL script.sql
@@ -21,7 +21,8 @@ There is no duplicated or null entries.
 
 1) Which genre has the most books in this category?
 
-select genre, count(genre) as count_genres
+select genre, 
+count(genre) as count_genres
 from amazon 
 group by genre
 
@@ -29,14 +30,16 @@ Fiction 240, Non-Fiction 310 books
 
 2) How do user ratings differ for each genre?
 
-select genre, "User Rating"
+select genre, 
+"User Rating"
 from amazon
 group by genre
 
 User rating for Fiction is 4.6, Non-Fiction 4.7 
 
 3) How does price differ for Fiction and Non Fiction books?
-select genre, sum(price)
+select genre, 
+sum(price)
 from amazon a 
 group by genre
 
@@ -46,7 +49,9 @@ Non Fiction	4601
 
 How does price differ for Fiction and Non Fiction books over years?
 
-select genre, year, price
+select genre, 
+year, 
+price
 from amazon a 
 group by year, genre
 order by genre desc;
@@ -67,9 +72,9 @@ Non Fiction	2019	12	|  Fiction	2019	5
 
 4) Which genres have the highest and lowest ratings?
 
-select max("user rating") as highest_rating, 
-	   min("user rating") as lowest_rating, 
-genre
+select genre,
+max("user rating") as highest_rating, 
+min("user rating") as lowest_rating
 from amazon
 where genre = 'Non Fiction' or genre = 'Fiction'
 group by genre;
@@ -77,7 +82,9 @@ group by genre;
 
 5) Which genre is most popular and which have earned the most in this category?
 
-select genre, max("User Rating") as max_rating, sum(price) as total_earnings
+select genre, 
+max("User Rating") as max_rating, 
+sum(price) as total_earnings
 from amazon
 group by genre
 order by total_earnings desc, max_rating desc;
@@ -85,14 +92,17 @@ order by total_earnings desc, max_rating desc;
 
 6) Which books have earned the most money in each genre?
 
-select genre, sum(price)
+select genre, 
+sum(price)
 from amazon
 group by genre
 
 
 7) Price of books of each genre over the years.
 
-select genre, price, "year"
+select genre, 
+price, 
+"year"
 from amazon
 group by genre, "year"
 order by "year";
@@ -100,7 +110,9 @@ order by "year";
 
 8) How has been the customer reviews by genre over the years?
 
-select genre, reviews, "year"
+select genre, 
+reviews, 
+"year"
 from amazon
 group by genre, "year"  
 order by year
@@ -108,14 +120,19 @@ order by year
 
 9) The price range of books by each genre in the Bestsellers list
 
-select genre, min(price) as min_price, max(price) as max_price, avg(price) as avg_price
+select genre, 
+min(price) as min_price,
+max(price) as max_price, 
+avg(price) as avg_price
 from amazon
 group by genre
 
 
 10) Reviews of books by genre for each year
 
-select genre, "year", reviews
+select genre, 
+"year", 
+reviews
 from amazon
 group by genre, "year"
 order by "year" asc, genre asc;
@@ -123,7 +140,9 @@ order by "year" asc, genre asc;
 
 11) The number of books by each genre over years
 
-select genre, "year", count(*) as number_books
+select genre, 
+"year", 
+count(*) as number_books
 from amazon
 group by genre, "year"
 order by "year" asc, genre asc;
@@ -131,7 +150,9 @@ order by "year" asc, genre asc;
 
 12) Amazon top 50 books by user rating from 2009-19
 
-select name, "User Rating", year
+select name, 
+"User Rating", 
+year
 from amazon
 group by "User Rating", year
 order by "User Rating" desc
